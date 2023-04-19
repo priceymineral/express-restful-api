7e0d694ec9f19d53b34958e0a97c0a47fc12d578
Octavio Sifuentes <tudaeva@gmail.com>
2023-04-19 16:37:32 -0700

    set up basic express server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
+
+// This code sets up a basic Express.js server that listens on port 3000 and responds with "Hello World!" when a GET request is made to the root endpoint. You can test this by running the node index.js command in your terminal and opening http://localhost:3000in your browser.
