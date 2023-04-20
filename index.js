c2fc9bb7f465c44e629f0abe7d3b98031e37df75
Octavio Sifuentes <tudaeva@gmail.com>
2023-04-19 17:00:47 -0700

    added get, post, put and delete routes

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/api/users', (req, res) => {
+  res.send('List of users here');
+});
+
+app.post('/api/users', (req, res) => {
+  res.send('Post a new user here');
+});
+
+app.put('/api/users/:id', (req, res) => {
+  res.send('Update user by ID');
+});
+
+app.delete('/api/users/:id', (req, res) => {
+  res.send('Delete user by ID');
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}.`);
 });
 
-// This code sets up a basic Express.js server that listens on port 3000 and responds with "Hello World!" when a GET request is made to the root endpoint. You can test this by running the node index.js command in your terminal and opening http://localhost:3000in your browser.
+// This code sets up a basic Express.js server that listens on port 3000 and responds with "Hello World!" when a GET request is made to the root endpoint. You can test this by running the node index.js command in your terminal and opening http://localhost:3000 in your browser.
