03cde71629e2c9bd4319047fedbf52f958467e17
Octavio Sifuentes <tudaeva@gmail.com>
2023-05-15 19:44:04 -0700

    typescript integration

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import express, { Express, Request, Response } from 'express';
+const port = 3000;
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Tamago! 💛");
+});
+
+app.get("/hi", (req: Request, res: Response) => {
+  res.send("Hiiiiiiiii");
+});
+
+app.listen(port, () => {
+  console.log(`✅ Server listening on port ${port}.`);
+});
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Tamago Pan! 💛');
+// });
+
+// app.get('/api/users', (req, res) => {
+//   res.send('List of users here');
+// });
+
+// app.post('/api/users', (req, res) => {
+//   res.send('Post a new user here');
+// });
+
+// app.put('/api/users/:id', (req, res) => {
+//   res.send('Update user by ID');
+// });
+
+// app.delete('/api/users/:id', (req, res) => {
+//   res.send('Delete user by ID');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}.`);
+// });
+
+// This code sets up a basic Express.js server that listens on port 3000 and responds with "Hello World!" when a GET request is made to the root endpoint. You can test this by running the node index.js command in your terminal and opening http://localhost:3000 in your browser.
+
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/exprestfulapi');
+
+// const userSchema = new mongoose.Schema({
+//   name: String,
+//   email: String,
+//   password: String
+// });
+
+// const User = mongoose.model('User', userSchema);
